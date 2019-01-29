a90b90d2761ef7f66b2c67f96b438a75747b8bc0
Abraham Taewoo Kim <akim92@gsu.edu>
2019-01-29 16:22:17 -0500

    v1 musica

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,77 @@
+import { MusicProvider } from './../../providers/music/music';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, ActionSheetController } from 'ionic-angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public allMusic = [];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    private socialSharing: SocialSharing,
+    private actionSheetController: ActionSheetController,
+    private loadingController: LoadingController, 
+    public navCtrl: NavController, 
+    private musicProvider: MusicProvider) {
+    
   }
 
+  ionViewDidLoad(){
+    let allMusicLoadingController = this.loadingController.create({
+      content: "Getting your Music From Server"
+    });
+    allMusicLoadingController.present();
+    this.musicProvider.getMusic()
+      .subscribe((musicList) => {
+        allMusicLoadingController.dismiss();
+        this.allMusic = musicList
+      });
+  }
+
+  addOneSong(refresher){
+    this.musicProvider.getOneSong()
+      .subscribe((oneSong) => {
+        this.allMusic.unshift(oneSong[0]);
+        refresher.complete();
+      });
+  }
+
+  shareSong(music){
+    let shareSongActionSheet = this.actionSheetController.create({
+      title: "Share Song With Friends",
+      buttons: [
+        {
+          text: "Share On Facebook",
+          icon: "logo-facebook",
+          handler: () => {
+            this.socialSharing.shareViaFacebook(music.name, music.image, music.music_url)
+          }
+        },
+        {
+          text: "Share On Twitter",
+          icon: "logo-twitter",
+          handler: () => {
+            this.socialSharing.shareViaTwitter(music.name, music.image, music.music_url)
+          }
+        },
+        {
+          text: "Share",
+          icon: "share",
+          handler: () => {
+            this.socialSharing.share(music.name, "", music.image, music.music_url)
+          }
+        },
+        {
+          text: "Cancel",
+          role: "destructive"
+        }
+      ]
+
+    });
+    shareSongActionSheet.present();
+  }
 }
